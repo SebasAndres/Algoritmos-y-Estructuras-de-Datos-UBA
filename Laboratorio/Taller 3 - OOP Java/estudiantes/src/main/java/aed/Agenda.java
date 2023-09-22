501793c0ba9b08691247195eddc7506ef144a828
Sebastian Andres <sebastian.ignacio.andres@gmail.com>
2023-09-22 17:14:04 -0300

    agrego semana tads

---
+++ Laboratorio/Taller 3 - OOP Java/estudiantes/src/main/java/aed/Agenda.java
@@ -0,0 +1,31 @@
+package aed;
+
+import java.util.Vector;
+
+public class Agenda {
+
+    public Agenda(Fecha fechaActual) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void agregarRecordatorio(Recordatorio recordatorio) {
+        throw new UnsupportedOperationException("No implementada aun");
+
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("No implementada aun");
+
+    }
+
+    public void incrementarDia() {
+        throw new UnsupportedOperationException("No implementada aun");
+
+    }
+
+    public Fecha fechaActual() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+}
---
+++ Laboratorio/Taller 3 - OOP Java/estudiantes/src/main/java/aed/VectorDeInts.java
@@ -0,0 +1,38 @@
+package aed;
+
+class VectorDeInts implements SecuenciaDeInts {
+    private static final int CAPACIDAD_INICIAL = 1;
+
+    public VectorDeInts() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public VectorDeInts(VectorDeInts vector) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public int longitud() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void agregarAtras(int i) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public int obtener(int i) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void quitarAtras() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void modificarPosicion(int indice, int valor) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public VectorDeInts copiar() {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+}
